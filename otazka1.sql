b70ca7960cee713d3f0e812dc9591dee8a76e525
Tereza Bohmova <tereza.pomi@gmail.com>
2025-06-27 19:24:16 +0200

    Formální úpravy

--- otazka1.sql
+++ otazka1.sql
@@ -1,43 +1,47 @@
 --OTÁZKA 1:Rostou v průběhu let mzdy ve všech odvětvích, nebo v některých klesají?
 
---skript1: zobrazení odvětví, kde došlo k poklesu mzdy mezi roky
+--Skript 1: zobrazení odvětví, kde došlo k poklesu mzdy mezi roky
 
 WITH payroll_comparison AS ( 
-  SELECT tb1.INDUSTRY_BRANCH_CODE, tb1.INDUSTRY_BRANCH_NAME, tb1.PAYROLL_YEAR, AVG(value) AS avg_payroll,
-      LAG(tb1.payroll_year) OVER (PARTITION BY tb1.industry_branch_code ORDER BY tb1.PAYROLL_YEAR) previous_payroll_year,
-      AVG(value) - lag(AVG(value)) OVER (PARTITION BY tb1.industry_branch_code ORDER BY tb1.PAYROLL_YEAR) payroll_change
-FROM T_TEREZA_BOHMOVA_PROJECT_SQL_PRIMARY_FINAL tb1
-WHERE tb1.VALUE_TYPE_CODE = '5958' AND tb1.CALCULATION_CODE = '200'
-GROUP BY tb1.INDUSTRY_BRANCH_CODE, tb1.INDUSTRY_BRANCH_NAME, tb1.PAYROLL_YEAR
-)
+           SELECT tb1.INDUSTRY_BRANCH_CODE, tb1.INDUSTRY_BRANCH_NAME, 
+                  tb1.PAYROLL_YEAR, AVG(value) AS avg_payroll,
+                  LAG(tb1.payroll_year) OVER (PARTITION BY tb1.industry_branch_code ORDER BY tb1.PAYROLL_YEAR) previous_payroll_year,
+                  AVG(value) - lag(AVG(value)) OVER (PARTITION BY tb1.industry_branch_code ORDER BY tb1.PAYROLL_YEAR) payroll_change
+           FROM T_TEREZA_BOHMOVA_PROJECT_SQL_PRIMARY_FINAL tb1
+           WHERE tb1.VALUE_TYPE_CODE = '5958' AND tb1.CALCULATION_CODE = '200'
+           GROUP BY tb1.INDUSTRY_BRANCH_CODE, tb1.INDUSTRY_BRANCH_NAME, tb1.PAYROLL_YEAR
+           )
 SELECT DISTINCT pc.INDUSTRY_BRANCH_NAME 
 FROM payroll_comparison pc
 WHERE pc.payroll_change < 0;
 
---skript2: komplexnější tabulka - přehled odvětví a roků, kdy došlo k poklesu mzdy, seřazeno od největšího poklesu po nejmenší
+--Skript 2: komplexnější tabulka - přehled odvětví a roků, kdy došlo k poklesu mzdy, seřazeno od největšího poklesu po nejmenší
+
 WITH payroll_comparison AS ( 
-  SELECT tb1.INDUSTRY_BRANCH_CODE, tb1.INDUSTRY_BRANCH_NAME, tb1.PAYROLL_YEAR, AVG(value) AS avg_payroll,
-      LAG(tb1.payroll_year) OVER (PARTITION BY tb1.industry_branch_code ORDER BY tb1.PAYROLL_YEAR) previous_payroll_year,
-      AVG(value) - lag(AVG(value)) OVER (PARTITION BY tb1.industry_branch_code ORDER BY tb1.PAYROLL_YEAR) payroll_change
-FROM T_TEREZA_BOHMOVA_PROJECT_SQL_PRIMARY_FINAL tb1
-WHERE tb1.VALUE_TYPE_CODE = '5958' AND tb1.CALCULATION_CODE = '200'
-GROUP BY tb1.INDUSTRY_BRANCH_CODE, tb1.INDUSTRY_BRANCH_NAME, tb1.PAYROLL_YEAR
-)
+                SELECT tb1.INDUSTRY_BRANCH_CODE, tb1.INDUSTRY_BRANCH_NAME, 
+                       tb1.PAYROLL_YEAR, AVG(value) AS avg_payroll,
+                       LAG(tb1.payroll_year) OVER (PARTITION BY tb1.industry_branch_code ORDER BY tb1.PAYROLL_YEAR) previous_payroll_year,
+                       AVG(value) - lag(AVG(value)) OVER (PARTITION BY tb1.industry_branch_code ORDER BY tb1.PAYROLL_YEAR) payroll_change
+                FROM T_TEREZA_BOHMOVA_PROJECT_SQL_PRIMARY_FINAL tb1
+                WHERE tb1.VALUE_TYPE_CODE = '5958' AND tb1.CALCULATION_CODE = '200'
+                GROUP BY tb1.INDUSTRY_BRANCH_CODE, tb1.INDUSTRY_BRANCH_NAME, tb1.PAYROLL_YEAR
+                )
 SELECT *
 FROM payroll_comparison pc
 WHERE pc.payroll_change < 0
 ORDER BY pc.payroll_change;
 
---skript 3: U kolika odvětví došlo v daném roce k poklesu mzdy? 
+--Skript 3: U kolika odvětví došlo v daném roce k poklesu mzdy? 
 
 WITH payroll_comparison AS ( 
-  SELECT tb1.INDUSTRY_BRANCH_CODE, tb1.INDUSTRY_BRANCH_NAME, tb1.PAYROLL_YEAR, AVG(value) AS avg_payroll,
-      LAG(tb1.payroll_year) OVER (PARTITION BY tb1.industry_branch_code ORDER BY tb1.PAYROLL_YEAR) previous_payroll_year,
-      AVG(value) - lag(AVG(value)) OVER (PARTITION BY tb1.industry_branch_code ORDER BY tb1.PAYROLL_YEAR) payroll_change
-FROM T_TEREZA_BOHMOVA_PROJECT_SQL_PRIMARY_FINAL tb1
-WHERE tb1.VALUE_TYPE_CODE = '5958' AND tb1.CALCULATION_CODE = '200'
-GROUP BY tb1.INDUSTRY_BRANCH_CODE, tb1.INDUSTRY_BRANCH_NAME, tb1.PAYROLL_YEAR
-)
+                 SELECT tb1.INDUSTRY_BRANCH_CODE, tb1.INDUSTRY_BRANCH_NAME, 
+                        tb1.PAYROLL_YEAR, AVG(value) AS avg_payroll,
+                        LAG(tb1.payroll_year) OVER (PARTITION BY tb1.industry_branch_code ORDER BY tb1.PAYROLL_YEAR) previous_payroll_year,
+                        AVG(value) - lag(AVG(value)) OVER (PARTITION BY tb1.industry_branch_code ORDER BY tb1.PAYROLL_YEAR) payroll_change
+                 FROM T_TEREZA_BOHMOVA_PROJECT_SQL_PRIMARY_FINAL tb1
+                 WHERE tb1.VALUE_TYPE_CODE = '5958' AND tb1.CALCULATION_CODE = '200'
+                 GROUP BY tb1.INDUSTRY_BRANCH_CODE, tb1.INDUSTRY_BRANCH_NAME, tb1.PAYROLL_YEAR
+                 )
 SELECT pc.payroll_year , count (*)
 FROM payroll_comparison pc
 WHERE pc.payroll_change < 0
@@ -45,17 +49,18 @@ GROUP BY pc.payroll_year
 ORDER BY count DESC;
 
 
---skript4: Kolikrát došlo za celé období v daném odvětvéí k poklesu mzdy?
+--Skript 4: Kolikrát došlo za celé období v daném odvětví k poklesu mzdy?
 
 WITH payroll_comparison AS ( 
-  SELECT tb1.INDUSTRY_BRANCH_CODE, tb1.INDUSTRY_BRANCH_NAME, tb1.PAYROLL_YEAR, AVG(value) AS avg_payroll,
-      LAG(tb1.payroll_year) OVER (PARTITION BY tb1.industry_branch_code ORDER BY tb1.PAYROLL_YEAR) previous_payroll_year,
-      AVG(value) - lag(AVG(value)) OVER (PARTITION BY tb1.industry_branch_code ORDER BY tb1.PAYROLL_YEAR) payroll_change
-FROM T_TEREZA_BOHMOVA_PROJECT_SQL_PRIMARY_FINAL tb1
-WHERE tb1.VALUE_TYPE_CODE = '5958' AND tb1.CALCULATION_CODE = '200'
-GROUP BY tb1.INDUSTRY_BRANCH_CODE, tb1.INDUSTRY_BRANCH_NAME, tb1.PAYROLL_YEAR
-)
-SELECT pc.industry_branch_name , count (*)
+                   SELECT tb1.INDUSTRY_BRANCH_CODE, tb1.INDUSTRY_BRANCH_NAME, 
+                         tb1.PAYROLL_YEAR, AVG(value) AS avg_payroll,
+                         LAG(tb1.payroll_year) OVER (PARTITION BY tb1.industry_branch_code ORDER BY tb1.PAYROLL_YEAR) previous_payroll_year,
+                         AVG(value) - lag(AVG(value)) OVER (PARTITION BY tb1.industry_branch_code ORDER BY tb1.PAYROLL_YEAR) payroll_change
+                   FROM T_TEREZA_BOHMOVA_PROJECT_SQL_PRIMARY_FINAL tb1
+                   WHERE tb1.VALUE_TYPE_CODE = '5958' AND tb1.CALCULATION_CODE = '200'
+                   GROUP BY tb1.INDUSTRY_BRANCH_CODE, tb1.INDUSTRY_BRANCH_NAME, tb1.PAYROLL_YEAR
+                    )
+SELECT pc.industry_branch_name, count (*)
 FROM payroll_comparison pc
 WHERE pc.payroll_change < 0
 GROUP BY pc.INDUSTRY_BRANCH_NAME
--- otazka3.sql
+++ otazka3.sql
@@ -1,34 +1,40 @@
 --OTÁZKA 3: Která kategorie potravin zdražuje nejpomaleji (je u ní nejnižší percentuální meziroční nárůst)? 
---Skript1 - preferovaná varianta, porovnání prvního a posledního roku ve vybraném období
+--Skript 1 - preferovaná varianta, porovnání prvního a posledního roku ve vybraném období
 
-SELECT date_part ('year', DATE_FROM ) AS food_price_year, tb1.FOOD_CODE , tb1.FOOD_NAME , AVG(tb1.FOOD_PRICE) AS avg_food_price,
+SELECT date_part ('year', DATE_FROM ) AS food_price_year, 
+       tb1.FOOD_CODE, tb1.FOOD_NAME, 
+       AVG(tb1.FOOD_PRICE) AS avg_food_price,
        LAG(date_part('year',date_from)) OVER (PARTITION BY tb1.FOOD_CODE ORDER BY date_part('year',date_from)) previous_year,
        AVG(tb1.FOOD_PRICE)/LAG(AVG(tb1.FOOD_PRICE)) OVER (PARTITION BY tb1.food_code ORDER BY date_part('year',date_from)) price_change  
 FROM T_TEREZA_BOHMOVA_PROJECT_SQL_PRIMARY_FINAL tb1
 WHERE date_part ('year', DATE_FROM ) IN (2006,2018)
 GROUP BY date_part ('year', DATE_FROM ), tb1.FOOD_CODE , tb1.FOOD_NAME 
-ORDER BY PRICE_CHANGE 
+ORDER BY PRICE_CHANGE; 
 
     --je nutno ale ověřit, že nezlevnilo víno (Skript1b)
 
-SELECT date_part ('year', DATE_FROM ) AS food_price_year, tb1.FOOD_CODE , tb1.FOOD_NAME , AVG(tb1.FOOD_PRICE) AS avg_food_price,
+SELECT date_part ('year', DATE_FROM ) AS food_price_year, 
+       tb1.FOOD_CODE, tb1.FOOD_NAME, 
+       AVG(tb1.FOOD_PRICE) AS avg_food_price,
        LAG(date_part('year',date_from)) OVER (PARTITION BY tb1.FOOD_CODE ORDER BY date_part('year',date_from)) previous_year,
        AVG(tb1.FOOD_PRICE)/LAG(AVG(tb1.FOOD_PRICE)) OVER (PARTITION BY tb1.food_code ORDER BY date_part('year',date_from)) price_change  
 FROM T_TEREZA_BOHMOVA_PROJECT_SQL_PRIMARY_FINAL tb1
 WHERE date_part ('year', DATE_FROM ) IN (2015,2018) AND tb1.FOOD_CODE = '212101'
-GROUP BY date_part ('year', DATE_FROM ), tb1.FOOD_CODE , tb1.FOOD_NAME 
+GROUP BY date_part ('year', DATE_FROM ), tb1.FOOD_CODE , tb1.FOOD_NAME; 
 
---Skript2 - další možný pohled (vychází ze skriptu výše, jen je tabulka za celé období a je vypočten průměr změny cen)
+--Skript 2 - další možný pohled (vychází ze skriptu výše, jen tabulka je za celé období a je vypočten průměr změny cen)
 
 WITH avg_price_change AS (
-SELECT date_part ('year', DATE_FROM ) AS food_price_year, tb1.FOOD_CODE AS fcode , tb1.FOOD_NAME AS fname , AVG(tb1.FOOD_PRICE) AS avg_food_price,
-       LAG(date_part('year',date_from)) OVER (PARTITION BY tb1.FOOD_CODE ORDER BY date_part('year',date_from)) previous_year,
-       AVG(tb1.FOOD_PRICE)/LAG(AVG(tb1.FOOD_PRICE)) OVER (PARTITION BY tb1.food_code ORDER BY date_part('year',date_from)) price_change  
-FROM T_TEREZA_BOHMOVA_PROJECT_SQL_PRIMARY_FINAL tb1
-GROUP BY date_part ('year', DATE_FROM ), tb1.FOOD_CODE , tb1.FOOD_NAME
-)
+                  SELECT date_part ('year', DATE_FROM ) AS food_price_year, 
+                         tb1.FOOD_CODE AS fcode, tb1.FOOD_NAME AS fname , 
+                         AVG(tb1.FOOD_PRICE) AS avg_food_price,
+                         LAG(date_part('year',date_from)) OVER (PARTITION BY tb1.FOOD_CODE ORDER BY date_part('year',date_from)) previous_year,
+                         AVG(tb1.FOOD_PRICE)/LAG(AVG(tb1.FOOD_PRICE)) OVER (PARTITION BY tb1.food_code ORDER BY date_part('year',date_from)) price_change  
+                  FROM T_TEREZA_BOHMOVA_PROJECT_SQL_PRIMARY_FINAL tb1
+                  GROUP BY date_part ('year', DATE_FROM ), tb1.FOOD_CODE, tb1.FOOD_NAME
+                  )
 SELECT apc.FCODE, apc.fname, avg(apc.PRICE_CHANGE) AS avg_price_change
 FROM avg_price_change apc
 GROUP BY apc.FCODE, apc.FNAME
-ORDER BY AVG_PRICE_CHANGE 
+ORDER BY AVG_PRICE_CHANGE; 
   
