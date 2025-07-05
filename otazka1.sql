13948f15e8deca0ebeae95d12138e3ffa71737c1
Tereza Bohmova <tereza.pomi@gmail.com>
2025-07-05 10:56:42 +0200

    Added values comment, formatting

--- otazka1.sql
+++ otazka1.sql
@@ -2,66 +2,94 @@
 
 --Skript 1: zobrazení odvětví, kde došlo k poklesu mzdy mezi roky
 
-WITH payroll_comparison AS ( 
-           SELECT tb1.INDUSTRY_BRANCH_CODE, tb1.INDUSTRY_BRANCH_NAME, 
-                  tb1.PAYROLL_YEAR, AVG(value) AS avg_payroll,
-                  LAG(tb1.payroll_year) OVER (PARTITION BY tb1.industry_branch_code ORDER BY tb1.PAYROLL_YEAR) previous_payroll_year,
-                  AVG(value) - lag(AVG(value)) OVER (PARTITION BY tb1.industry_branch_code ORDER BY tb1.PAYROLL_YEAR) payroll_change
-           FROM T_TEREZA_BOHMOVA_PROJECT_SQL_PRIMARY_FINAL tb1
-           WHERE tb1.VALUE_TYPE_CODE = '5958' AND tb1.CALCULATION_CODE = '200'
-           GROUP BY tb1.INDUSTRY_BRANCH_CODE, tb1.INDUSTRY_BRANCH_NAME, tb1.PAYROLL_YEAR
-           )
-SELECT DISTINCT pc.INDUSTRY_BRANCH_NAME 
+WITH payroll_comparison AS
+  (SELECT tb1.industry_branch_code, 
+          tb1.industry_branch_name, 
+          tb1.payroll_year, 
+          AVG(value) AS avg_payroll,
+          LAG(tb1.payroll_year) OVER (PARTITION BY tb1.industry_branch_code
+                                      ORDER BY tb1.PAYROLL_YEAR) previous_payroll_year,
+          AVG(value) - lag(AVG(value)) OVER (PARTITION BY tb1.industry_branch_code
+                                             ORDER BY tb1.PAYROLL_YEAR) payroll_change
+   FROM T_TEREZA_BOHMOVA_PROJECT_SQL_PRIMARY_FINAL tb1
+   WHERE tb1.value_type_code = '5958'    --průměrná hrubá mzda na zaměstnance
+     AND tb1.calculation_code = '200'   --mzda na přepočtený počet zaměstnanců
+   GROUP BY tb1.industry_branch_code, 
+            tb1.industry_branch_name,
+            tb1.payroll_year)   
+SELECT DISTINCT pc.industry_branch_name
 FROM payroll_comparison pc
 WHERE pc.payroll_change < 0;
 
+
 --Skript 2: komplexnější tabulka - přehled odvětví a roků, kdy došlo k poklesu mzdy, seřazeno od největšího poklesu po nejmenší
 
-WITH payroll_comparison AS ( 
-                SELECT tb1.INDUSTRY_BRANCH_CODE, tb1.INDUSTRY_BRANCH_NAME, 
-                       tb1.PAYROLL_YEAR, AVG(value) AS avg_payroll,
-                       LAG(tb1.payroll_year) OVER (PARTITION BY tb1.industry_branch_code ORDER BY tb1.PAYROLL_YEAR) previous_payroll_year,
-                       AVG(value) - lag(AVG(value)) OVER (PARTITION BY tb1.industry_branch_code ORDER BY tb1.PAYROLL_YEAR) payroll_change
-                FROM T_TEREZA_BOHMOVA_PROJECT_SQL_PRIMARY_FINAL tb1
-                WHERE tb1.VALUE_TYPE_CODE = '5958' AND tb1.CALCULATION_CODE = '200'
-                GROUP BY tb1.INDUSTRY_BRANCH_CODE, tb1.INDUSTRY_BRANCH_NAME, tb1.PAYROLL_YEAR
-                )
+WITH payroll_comparison AS
+  (SELECT tb1.industry_branch_code,
+          tb1.industry_branch_name,
+          tb1.payroll_year,
+          AVG(value) AS avg_payroll,
+          LAG(tb1.payroll_year) OVER (PARTITION BY tb1.industry_branch_code
+                                      ORDER BY tb1.PAYROLL_YEAR) previous_payroll_year,
+          AVG(value) - lag(AVG(value)) OVER (PARTITION BY tb1.industry_branch_code
+                                             ORDER BY tb1.PAYROLL_YEAR) payroll_change
+   FROM T_TEREZA_BOHMOVA_PROJECT_SQL_PRIMARY_FINAL tb1
+   WHERE tb1.value_type_code = '5958'   --průměrná hrubá mzda na zaměstnance
+     AND tb1.calculation_code = '200'   --mzda na přepočtený počet zaměstnanců
+   GROUP BY tb1.industry_branch_code,
+            tb1.industry_branch_name,
+            tb1.payroll_year)
 SELECT *
 FROM payroll_comparison pc
 WHERE pc.payroll_change < 0
 ORDER BY pc.payroll_change;
 
+
 --Skript 3: U kolika odvětví došlo v daném roce k poklesu mzdy? 
 
-WITH payroll_comparison AS ( 
-                 SELECT tb1.INDUSTRY_BRANCH_CODE, tb1.INDUSTRY_BRANCH_NAME, 
-                        tb1.PAYROLL_YEAR, AVG(value) AS avg_payroll,
-                        LAG(tb1.payroll_year) OVER (PARTITION BY tb1.industry_branch_code ORDER BY tb1.PAYROLL_YEAR) previous_payroll_year,
-                        AVG(value) - lag(AVG(value)) OVER (PARTITION BY tb1.industry_branch_code ORDER BY tb1.PAYROLL_YEAR) payroll_change
-                 FROM T_TEREZA_BOHMOVA_PROJECT_SQL_PRIMARY_FINAL tb1
-                 WHERE tb1.VALUE_TYPE_CODE = '5958' AND tb1.CALCULATION_CODE = '200'
-                 GROUP BY tb1.INDUSTRY_BRANCH_CODE, tb1.INDUSTRY_BRANCH_NAME, tb1.PAYROLL_YEAR
-                 )
-SELECT pc.payroll_year , count (*)
+WITH payroll_comparison AS
+  (SELECT tb1.industry_branch_code,
+          tb1.industry_branch_name,
+          tb1.payroll_year,
+          AVG(value) AS avg_payroll,
+          LAG(tb1.payroll_year) OVER (PARTITION BY tb1.industry_branch_code
+                                      ORDER BY tb1.PAYROLL_YEAR) previous_payroll_year,
+          AVG(value) - lag(AVG(value)) OVER (PARTITION BY tb1.industry_branch_code
+                                             ORDER BY tb1.PAYROLL_YEAR) payroll_change
+   FROM T_TEREZA_BOHMOVA_PROJECT_SQL_PRIMARY_FINAL tb1
+   WHERE tb1.value_type_code = '5958'     --průměrná hrubá mzda na zaměstnance
+     AND tb1.calculation_code = '200'     --mzda na přepočtený počet zaměstnanců
+   GROUP BY tb1.industry_branch_code,
+            tb1.industry_branch_name,
+            tb1.payroll_year)
+SELECT pc.payroll_year,
+       COUNT (*)
 FROM payroll_comparison pc
 WHERE pc.payroll_change < 0
 GROUP BY pc.payroll_year
-ORDER BY count DESC;
+ORDER BY COUNT DESC;
 
 
 --Skript 4: Kolikrát došlo za celé období v daném odvětví k poklesu mzdy?
 
-WITH payroll_comparison AS ( 
-                   SELECT tb1.INDUSTRY_BRANCH_CODE, tb1.INDUSTRY_BRANCH_NAME, 
-                         tb1.PAYROLL_YEAR, AVG(value) AS avg_payroll,
-                         LAG(tb1.payroll_year) OVER (PARTITION BY tb1.industry_branch_code ORDER BY tb1.PAYROLL_YEAR) previous_payroll_year,
-                         AVG(value) - lag(AVG(value)) OVER (PARTITION BY tb1.industry_branch_code ORDER BY tb1.PAYROLL_YEAR) payroll_change
-                   FROM T_TEREZA_BOHMOVA_PROJECT_SQL_PRIMARY_FINAL tb1
-                   WHERE tb1.VALUE_TYPE_CODE = '5958' AND tb1.CALCULATION_CODE = '200'
-                   GROUP BY tb1.INDUSTRY_BRANCH_CODE, tb1.INDUSTRY_BRANCH_NAME, tb1.PAYROLL_YEAR
-                    )
-SELECT pc.industry_branch_name, count (*)
+WITH payroll_comparison AS
+  (SELECT tb1.industry_branch_code,
+          tb1.industry_branch_name,
+          tb1.payroll_year,
+          AVG(value) AS avg_payroll,
+          LAG(tb1.payroll_year) OVER (PARTITION BY tb1.industry_branch_code
+                                      ORDER BY tb1.payroll_year) previous_payroll_year,
+          AVG(value) - lag(AVG(value)) OVER (PARTITION BY tb1.industry_branch_code
+                                             ORDER BY tb1.payroll_year) payroll_change
+   FROM T_TEREZA_BOHMOVA_PROJECT_SQL_PRIMARY_FINAL tb1
+   WHERE tb1.value_type_code = '5958'     --průměrná hrubá mzda na zaměstnance
+     AND tb1.calculation_code = '200'     --mzda na přepočtený počet zaměstnanců
+   GROUP BY tb1.industry_branch_code,
+            tb1.industry_branch_name,
+            tb1.payroll_year)
+SELECT pc.industry_branch_name,
+       COUNT (*)
 FROM payroll_comparison pc
 WHERE pc.payroll_change < 0
-GROUP BY pc.INDUSTRY_BRANCH_NAME
-ORDER BY count DESC;
+GROUP BY pc.industry_branch_name
+ORDER BY COUNT DESC;
