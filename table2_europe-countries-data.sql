4a8ca59e41866e19fd938fb22658b0c203ed2939
Tereza Bohmova <tereza.pomi@gmail.com>
2025-07-11 16:17:13 +0200

    Úprava formátu

--- table2_europe-countries-data.sql
+++ table2_europe-countries-data.sql
@@ -1,10 +1,10 @@
 --script na tvorbu tabulky s vybranými daty pro další evropské státy za stejné období jako tabulka 1
 
 CREATE TABLE t_tereza_bohmova_project_SQL_secondary_final AS ( 
-       SELECT e.COUNTRY, e."year", e.GDP, e.GINI, e.POPULATION
-       FROM ECONOMIES E 
-       WHERE (E."year" >=2006 AND e."year" <=2018) 
-             AND (e.country IN(SELECT c.COUNTRY
-                     FROM COUNTRIES C
-                     WHERE c.CONTINENT = 'Europe'))
+       SELECT e.country, e."year", e.gdp, e.gini, e.population
+       FROM economies e 
+       WHERE (e."year" >=2006 AND e."year" <=2018) 
+             AND (e.country IN(SELECT c.country
+                     FROM countries c
+                     WHERE c.continent = 'Europe'))
      );
