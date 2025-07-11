bb25a43efdefca7cfd78dc48c422fdbd56d8159e
Tereza Bohmova <tereza.pomi@gmail.com>
2025-07-11 16:31:47 +0200

    Úprava formátu

--- otazka2.sql
+++ otazka2.sql
@@ -1,14 +1,22 @@
 --OTÁZKA 2: Kolik je možné si koupit litrů mléka a kilogramů chleba za první a poslední srovnatelné období v dostupných datech cen a mezd?
 
-SELECT tb1.DATE_FROM, tb1.FOOD_NAME, tb1.FOOD_QUANTITY, 
-       tb1.FOOD_UNIT, 
-       round (AVG(tb1.FOOD_PRICE)::numeric,2) AS avg_price, 
-       tb1.value AS payroll,
-       round(tb1.value/AVG(tb1.FOOD_PRICE)::numeric,0) AS quantity_to_buy
+SELECT date_from,
+       food_name,
+       food_quantity,
+       food_unit,
+       round (AVG(food_price)::numeric, 2) AS avg_price,
+       value AS payroll,
+       round(value/AVG(food_price)::numeric, 0) AS quantity_to_buy
 FROM T_TEREZA_BOHMOVA_PROJECT_SQL_PRIMARY_FINAL tb1
-WHERE (to_char(tb1.DATE_FROM , 'YYYY-MM-DD') IN ('2006-01-02', '2018-12-10'))
-     AND tb1.FOOD_CODE IN ('114201','111301')  
-     AND tb1.INDUSTRY_BRANCH_CODE IS NULL
-     AND tb1.value_type_code = '5958'
-     AND tb1.CALCULATION_CODE = '100'    
-GROUP BY tb1.DATE_FROM, tb1.FOOD_NAME, tb1.FOOD_QUANTITY, tb1.FOOD_UNIT, tb1.value;
+WHERE (to_char(date_from, 'YYYY-MM-DD') IN ('2006-01-02',
+                                            '2018-12-10'))
+  AND food_code IN ('114201',   --mléko polotučné pasterované
+                    '111301')   -- chléb konzumní kmínový
+  AND industry_branch_code IS NULL
+  AND value_type_code = '5958'    -- průměrná hrubá mzda na zaměstnance
+  AND calculation_code = '100'    -- mzda přepočtená na fyzický počet zaměstnanců
+GROUP BY date_from,
+         food_name,
+         food_quantity,
+         food_unit,
+         value;
